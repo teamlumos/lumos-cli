fd52c7b13c80b0a06398d8eb65ab5bd9ebb7f0a9
Rikki Prince <rikki@lumosidentity.com>
2024-02-07 14:56:04 -0500

    formatted print out of apps and permissions

--- request/cli.py
+++ request/cli.py
@@ -1,5 +1,7 @@
 from typing import List, Optional
 import typer
+from uuid import UUID
+from tabulate import tabulate
 
 from request.client import Client, App, Permission
 
@@ -10,21 +12,23 @@ client = Client()
 @app.callback(invoke_without_command=True)
 def request(
     ctx: typer.Context,
-    app: Optional[int] = None,
-    user: Optional[int] = None,
-    permission: Optional[int] = None,
+    app: Optional[UUID] = None,
+    user: Optional[UUID] = None,
+    permission: Optional[UUID] = None,
     length: Optional[int] = None,
 ) -> None:
     if ctx.invoked_subcommand is None:
         # Validate parameters or interactively input them
         if not app:
-            app = typer.prompt("Enter the domain app id")
+            # app = typer.prompt("Enter the domain app id")
+            print("Select an app")
+            # apps: List[App] = client.get_appstore_apps()
         if not user:
             user = typer.prompt("Enter the user id")
         if not permission:
             permission = typer.prompt("Enter the permission id")
-        if not length:
-            length = typer.prompt("Enter the access length")
+        # if not length:
+        #     length = typer.prompt("Enter the access length")
         print(f"Requesting app {app} for {user} to access permission {permission} for {length} seconds")
         
 
@@ -32,17 +36,16 @@ def request(
 def list_permissions(
     app_id: str
 ) -> None:
-    print(app_id)
+    print(f"Listing permissions for app {app_id}\n")
     permissions: List[Permission] = client.get_app_requestable_permissions(app_id=app_id)
-    for permission in permissions:
-        print(permission)
+    print(tabulate([[permission.label, permission.id] for permission in permissions], headers=["Permission", "UUID"]), "\n")
 
 @app.command()
 def list_apps(
 ) -> None:
+    print("Listing all your apps...\n")
     apps: List[App] = client.get_appstore_apps()
-    for app in apps:
-        print(app)
+    print(tabulate([[app.user_friendly_label, app.id] for app in apps], headers=["App", "UUID"]), "\n")
 
 @app.command()
 def create_access_request() -> List[App]:
