0c1707ff1e820757dd7183853e432eaf78f09c84
Niamh Durfee <niamh@lumos.com>
2024-03-19 11:15:11 -0700

    Loop to check permissions

--- customer-secrets.sh
+++ customer-secrets.sh
@@ -1,6 +1,12 @@
 secrets() {
     username=$(lumos whoami --username | awk -F '@' '{print $1}')
     lumos request --app 8156588a-2c42-2785-a437-f6aebc7a6197 --permission-like $1 --length 7200 --reason $2 --for-me --wait
-    sleep 30
-    aws iam list-roles --query "Roles[?contains(Arn, '$username-$1')]"
+    sleep 25
+    arn=""
+    while $arn == "" ; do
+        sleep 5
+        query=$(aws iam list-roles --query "Roles[?contains(Arn, '$username-$1')]")
+        arn=$(echo $query | grep -o '"Arn": *"[^"]*"' | cut -d'"' -f4)
+        roleName=$(echo $query | grep -o '"RoleName": *"[^"]*"' | cut -d'"' -f4)
+    done
 }
