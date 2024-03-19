cc2b1f1ac60c0419f51a788d00b2e1b02974c4d3
Niamh Durfee <niamh@lumos.com>
2024-03-18 19:08:59 -0700

    Cleanup

--- customer-secrets.sh
+++ customer-secrets.sh
@@ -1,6 +1,6 @@
 secrets() {
-    USERNAME=$(lumos whoami | sed -n 's/.*(\([^@]*\)@.*/\1/p')
+    username=$(lumos whoami --username | awk -F '@' '{print $1}')
     lumos request --app 8156588a-2c42-2785-a437-f6aebc7a6197 --permission-like $1 --length 7200 --reason $2 --for-me --wait
     sleep 30
-    aws iam list-roles --query "Roles[?contains(Arn, '$USERNAME-$1')]"
+    aws iam list-roles --query "Roles[?contains(Arn, '$username-$1')]"
 }
