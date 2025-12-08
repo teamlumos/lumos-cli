36ed59e206cfe954966e10f59fb6f83d05623e55
Jason D'Amour <jasondamour98@gmail.com>
2025-12-08 23:48:29 +0000

    feat: improve releases
    - get package version from metadata
    - upload built binaries to release page

--- package.sh
+++
@@ -1,8 +0,0 @@
-uv run pyinstaller lumos/__main__.py
-cd ./dist/__main__
-mv __main__ lumos
-tar -czf lumos.tar.gz *
-FILE_SHA=$(sha256sum lumos.tar.gz | cut -d ' ' -f1)
-echo *******************
-echo File SHA: $FILE_SHA
-echo *******************
